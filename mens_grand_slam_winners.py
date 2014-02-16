e0abb8efbfc4868a69d67aef72a25a5374d63e04
Fred Zhao <fred.y.zhao@gmail.com>
2014-02-15 18:23:31 -0800

    2009.
    Nadal wins his first Australian Open. Federer finally wins the French Open!
    Nadal disappointedly failed to defend his Wimbledon title, so made a note there
    to highlight this. del Potro wins the US Open in epic fashion in a time when
    Federer still looks to be on the top of his game.

--- mens_grand_slam_winners.py
+++ mens_grand_slam_winners.py
@@ -5,14 +5,30 @@
 '''
 
 
-# 2008
+# 2009
 def print_winner(tournament, year):
-    if tournament == 'Wimbledon' and year == 2008:
-        ''' Greatest Tennis Match in History '''
-        winner = 'Nadal'
+    if tournament == 'Wimbledon':
+        if year == 2008:
+            ''' Greatest Tennis Match in History '''
+            winner = 'Nadal'
+        else:
+            ''' Nadal couldn't defend his title after withdrawing due to
+                injuries during the first round.
+            '''
+            winner = 'Federer'
     elif tournament == 'French Open':
         if year == 2004:
             winner = 'Gaudio'
+        elif year == 2009:
+            ''' The term "Soderlinged" was coined after the man who went up
+                against Nadal in the fourth round and played a phenomenal match
+                to pull off a tremendous upset. Soderling would eventually go on
+                to reach the finals, only to lose to Federer. To date, this
+                remains the only year Federer claimed the French Open title, as
+                well as the only loss for Nadal at the French Open **since his
+                debut in 2005**.
+            '''
+            winner = 'Federer'
         else:
             winner = 'Nadal'
     elif tournament == 'Australian Open':
@@ -20,6 +36,19 @@ def print_winner(tournament, year):
             winner = 'Safin'
         elif year == 2008:
             winner = 'Djokovic'
+        elif year == 2009:
+            winner = 'Nadal'
+        else:
+            winner = 'Federer'
+    elif tournament == 'US Open':
+        if year == 2009:
+            ''' After Federer handily overcame Djokovic in the semifinals --
+                hitting one of his most famous shots of all time
+                (http://www.youtube.com/watch?v=nQ7HvffZpRI) -- he was beaten in
+                an epic 5 setter in the finals by del Potro, who had never
+                bested Federer in their head-to-head record thus far.
+            '''
+            winner = 'del Potro'
         else:
             winner = 'Federer'
     else:

654aa8f4580de1070df860d0da2447bfdd60e240
Fred Zhao <fred.y.zhao@gmail.com>
2014-02-16 02:20:49 -0800

    2012 - 2013. Improvement 3.
    Refactored the winner-determining function into its own helper to take advantage
    of early returns. This allows for changing the comment-only branches (such as
    the 2009 Wimbledon commentary) to truly do nothing. Converted all `if` branches
    so they must return, and now there's no more need of top-level `elif` uses. Also
    added attempted explanations for each top-level `if` block -- because that tells
    a much more interesting story about the modern men's tennis scene than a bunch
    of nested `if` statements.
    + One fewer `else` clause for French Open (implied by return at end of block)
    + One fewer `else` clause for 2010 (implied by return at end of block)
    + One fewer `else` clause for Wimbledon (implied by return at end of block)
    + One fewer `else` clause for Australian Open (implied by return at end of block)
    + One fewer `else` clause for US Open (implied by return at end of block)

--- mens_grand_slam_winners.py
+++ mens_grand_slam_winners.py
@@ -7,9 +7,15 @@
 
 # 2012 - 2013
 def print_winner(tournament, year):
+    winner = get_winner(tournament, year)
+    print 'The %s of %d was won by %s.' % (tournament, year, winner)
+
+
+def get_winner(tournament, year):
+    ''' "Nadal will almost always win the French Open" '''
     if tournament == 'French Open':
         if year == 2004:
-            winner = 'Gaudio'
+            return 'Gaudio'
         elif year == 2009:
             ''' The term "Soderlinged" was coined after the man who went up
                 against Nadal in the fourth round and played a phenomenal match
@@ -19,10 +25,13 @@ def print_winner(tournament, year):
                 well as the only loss for Nadal at the French Open **since his
                 debut in 2005**.
             '''
-            winner = 'Federer'
-        else:
-            winner = 'Nadal'
-    elif year == 2010:
+            return 'Federer'
+        return 'Nadal'
+
+    ''' "2010 and 2011 saw Nadal and Djokovic (repectively) being basically
+        unstoppable"
+    '''
+    if year == 2010:
         ''' This was a dominant year for Nadal, with the exception of the
             Australian Open when he retired from injuries in the quarterfinals
             against Murray. Murray would be beaten by Federer in the finals, in
@@ -30,10 +39,9 @@ def print_winner(tournament, year):
             playing style was admittedly also to blame).
         '''
         if tournament == 'Australian Open':
-            winner = 'Federer'
-        else
-            winner = 'Nadal'
-    elif year == 2011:
+            return 'Federer'
+        return 'Nadal'
+    if year == 2011:
         ''' A similarly dominant year for Djokovic. After beating Tsonga in the
             Wimbledon seminfinals, he ascended to the ATP #1 spot for the first
             time in his career. However, in the semifinals of the French Open,
@@ -44,16 +52,20 @@ def print_winner(tournament, year):
             and breaking back to win the decisive fifth set, then went on to
             beat Nadal in the finals over four sets.
         '''
-        winner = 'Djokovic'
-    elif tournament == 'Wimbledon':
+        return 'Djokovic'
+
+    ''' "Wimbledon has been Federer's best tournament, with a few exceptions
+        that were truly, well... exceptional"
+    '''
+    if tournament == 'Wimbledon':
         if year == 2008:
             ''' Greatest Tennis Match in History '''
-            winner = 'Nadal'
+            return 'Nadal'
         elif year == 2009:
             ''' Nadal couldn't defend his title after withdrawing due to
                 injuries during the first round.
             '''
-            winner = 'Federer'
+            pass
         elif year == 2012:
             ''' Federer beat Murray in 4 sets in the finals for a record seven
                 Wimbledon titles (tying Sampras) and returning to the ATP #1
@@ -64,7 +76,7 @@ def print_winner(tournament, year):
                 played on the very same court where Murray lost to Federer a
                 month ago!
             '''
-            winner = 'Federer'
+            pass
         elif year == 2013:
             ''' After winning the gold medal in the 2012 Olympics, Murray found
                 the confidence he had been missing. He went on to win the 2012
@@ -72,21 +84,28 @@ def print_winner(tournament, year):
                 finals and became the first British man to do so since 1936 (as
                 well as the first Scot to do so since 1896).
             '''
-            winner = 'Murray'
-        else:
-            winner = 'Federer'
-    elif tournament == 'Australian Open':
+            return 'Murray'
+        return 'Federer'
+
+    ''' "The Australian has been hotly contested, with Federer and Djokovic
+        splitting a majority of the titles between the two of them -- with
+        Djokovic being the most recent to dominate the tournament"
+    '''
+    if tournament == 'Australian Open':
         if year == 2005:
-            winner = 'Safin'
+            return 'Safin'
         elif year == 2008:
-            winner = 'Djokovic'
+            pass
         elif year == 2009:
-            winner = 'Nadal'
+            return 'Nadal'
         elif year < 2011:
-            winner = 'Federer'
-        else:
-            winner = 'Djokovic'
-    elif tournament == 'US Open':
+            return 'Federer'
+        return 'Djokovic'
+
+    ''' "The US Open was the second best tournament for Federer, though after
+        losing to del Potro in 2009 he has yet to win the title again"
+    '''
+    if tournament == 'US Open':
         if year == 2009:
             ''' After Federer handily overcame Djokovic in the semifinals --
                 hitting one of his most famous shots of all time
@@ -94,15 +113,12 @@ def print_winner(tournament, year):
                 an epic 5 setter in the finals by del Potro, who had never
                 bested Federer in their head-to-head record thus far.
             '''
-            winner = 'del Potro'
+            return 'del Potro'
         elif year == 2012:
-            winner = 'Murray'
+            return 'Murray'
         elif year == 2013:
-            winner = 'Nadal' # I watched the finals live in New York City!
-        else:
-            winner = 'Federer'
-
-    print 'The %s of %d was won by %s.' % (tournament, year, winner)
+            return 'Nadal' # I watched the finals live in New York City!
+        return 'Federer'
 
 
 if __name__ == '__main__':
